61044ee5b1d0a6bd6960a59daf261fb47cfccfbd
Jolim Chiu <jolimchiuedu@gmail.com>
2026-06-19 17:09:07 -0500

    Initial commit

---
+++ Resilient_AMR_Dispatch/src/resilient_amr_dispatch/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+package_name = "resilient_amr_dispatch"
+
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=("test", "tests")),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="jchiu",
+    maintainer_email="jchiu@example.com",
+    description="Resilient warehouse AMR dispatch simulation.",
+    license="Apache-2.0",
+)
---
+++ Resilient_AMR_Dispatch/src/resilient_amr_dispatch/tests/test_package.py
@@ -0,0 +1,4 @@
+def test_package_imports() -> None:
+    import resilient_amr_dispatch
+
+    assert resilient_amr_dispatch.__doc__
